e10814dea3b1581d84daeae0bb5adf5bd63a0231
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-08-15 18:46:44 +0300

    Changed DLL loader function.

--- sdl3/__init__.py
+++ sdl3/__init__.py
@@ -1,10 +1,10 @@
 import ctypes, sys, os
 
-BINARY_PATH = os.path.join(os.path.dirname(__file__), "bin")
-SDL_DLL = ctypes.CDLL(os.path.join(BINARY_PATH, "SDL3.dll"))
+binaryPath = os.path.join(os.path.dirname(__file__), "bin")
+dll = ctypes.windll.LoadLibrary(os.path.join(binaryPath, "SDL3.dll"))
 
 def SDL_FUNC(name, restype, *args):
-    func = getattr(SDL_DLL, name)
+    func = getattr(dll, name)
     func.restype, func.argtypes = restype, args
     setattr(sys.modules[__name__.split(".")[0]], name, func)
 
