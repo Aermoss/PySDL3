4e904f41cb08255b2f6cbe60fc70a08187f5812f
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-08-19 16:02:40 +0300

    Added SDL_metal.h implementation.

---
+++ sdl3/SDL_metal.py
@@ -0,0 +1,9 @@
+from .__init__ import ctypes, SDL_FUNC
+
+from .SDL_video import SDL_Window
+
+SDL_MetalView = ctypes.c_void_p
+
+SDL_FUNC("SDL_Metal_CreateView", SDL_MetalView, ctypes.POINTER(SDL_Window))
+SDL_FUNC("SDL_Metal_DestroyView", None, SDL_MetalView)
+SDL_FUNC("SDL_Metal_GetLayer", ctypes.c_void_p, SDL_MetalView)
