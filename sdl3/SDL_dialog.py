50ccbc95d8166d7abe33f7893b087aba9dee38dd
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-08-19 16:01:38 +0300

    Added SDL_dialog.h implementation.

---
+++ sdl3/SDL_dialog.py
@@ -0,0 +1,15 @@
+from .__init__ import ctypes, SDL_FUNC
+
+from .SDL_video import SDL_Window
+
+class SDL_DialogFileFilter(ctypes.Structure):
+    _fields_ = [
+        ("name", ctypes.c_char_p),
+        ("pattern", ctypes.c_char_p)
+    ]
+
+SDL_DialogFileCallback = ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.POINTER(ctypes.c_char_p), ctypes.c_int)
+
+SDL_FUNC("SDL_ShowOpenFileDialog", None, SDL_DialogFileCallback, ctypes.c_void_p, ctypes.POINTER(SDL_Window), ctypes.POINTER(SDL_DialogFileFilter), ctypes.c_int, ctypes.c_char_p, ctypes.c_bool)
+SDL_FUNC("SDL_ShowSaveFileDialog", None, SDL_DialogFileCallback, ctypes.c_void_p, ctypes.POINTER(SDL_Window), ctypes.POINTER(SDL_DialogFileFilter), ctypes.c_int, ctypes.c_char_p)
+SDL_FUNC("SDL_ShowOpenFolderDialog", None, SDL_DialogFileCallback, ctypes.c_void_p, ctypes.POINTER(SDL_Window), ctypes.c_char_p, ctypes.c_bool)
