7d3c2ab1e786e42ac09b58f71000d20fafa0fe44
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-08-22 00:45:57 +0300

    Added SDL_system.h implementation.

---
+++ sdl3/SDL_system.py
@@ -0,0 +1,32 @@
+from .__init__ import sys, ctypes, wintypes, \
+    SDL_FUNC, SDL_SET_CURRENT_DLL, SDL_DLL
+
+SDL_SET_CURRENT_DLL(SDL_DLL)
+
+from .SDL_video import SDL_DisplayID
+
+if "win32" in sys.platform:
+    MSG = wintypes.tagMSG
+    SDL_WindowsMessageHook = ctypes.CFUNCTYPE(ctypes.c_bool, ctypes.c_void_p, ctypes.POINTER(MSG))
+    SDL_FUNC("SDL_SetWindowsMessageHook", None, SDL_WindowsMessageHook, ctypes.c_void_p)
+
+    SDL_FUNC("SDL_GetDirect3D9AdapterIndex", ctypes.c_int, SDL_DisplayID)
+    SDL_FUNC("SDL_GetDXGIOutputInfo", ctypes.c_int, SDL_DisplayID, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
+
+    class XEvent(ctypes.Union):
+        ...
+
+    SDL_X11EventHook = ctypes.CFUNCTYPE(ctypes.c_bool, ctypes.c_void_p, ctypes.POINTER(XEvent))
+    SDL_FUNC("SDL_SetX11EventHook", None, SDL_X11EventHook, ctypes.c_void_p) 
+
+if "linux" in sys.platform:
+    SDL_FUNC("SDL_SetLinuxThreadPriority", ctypes.c_int, ctypes.c_int64, ctypes.c_int)
+    SDL_FUNC("SDL_SetLinuxThreadPriorityAndPolicy", ctypes.c_int, ctypes.c_int64, ctypes.c_int, ctypes.c_int)
+
+SDL_FUNC("SDL_IsTablet", ctypes.c_bool)
+SDL_FUNC("SDL_OnApplicationWillTerminate", None)
+SDL_FUNC("SDL_OnApplicationDidReceiveMemoryWarning", None)
+SDL_FUNC("SDL_OnApplicationWillEnterBackground", None)
+SDL_FUNC("SDL_OnApplicationDidEnterBackground", None)
+SDL_FUNC("SDL_OnApplicationWillEnterForeground", None)
+SDL_FUNC("SDL_OnApplicationDidEnterForeground", None)
