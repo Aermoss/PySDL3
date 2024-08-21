24e4844cd5d19915a9f698abafd0e9de2b9444b2
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-08-22 01:18:10 +0300

    Updated version from 0.4.0a2 to 0.4.0a3.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def main(argv):
     data = {
         "name": "PySDL3",
         "packages": ["sdl3", "sdl3.bin"],
-        "version": "0.4.0a2",
+        "version": "0.4.0a3",
         "description": "A pure Python wrapper for SDL3.",
         "long_description": open("README.md", "r", encoding = "utf-8").read(),
         "long_description_content_type": "text/markdown",
