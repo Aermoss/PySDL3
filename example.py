7588888225a586d8942aae901ea5500ac7509525
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-08-23 11:59:02 +0300

    Added code to test new functions.

--- example.py
+++ example.py
@@ -1,7 +1,18 @@
 import sdl3, ctypes, os, \
     sys, colorsys, time
 
-def main(argv):
+def countLines() -> int:
+    lines = 0
+
+    for i in os.listdir("sdl3"):
+        if i.endswith(".py"):
+            with open(f"sdl3/{i}", "r") as file:
+                lines += file.read().count("\n") + 1
+
+    return lines
+
+def main(argv: list[str]) -> int:
+    # print(f"total lines of code: {countLines()}.")
     print(f"loaded {sum(len(v) for k, v in sdl3.functions.items())} functions.")
     result = sdl3.SDL_Init(sdl3.SDL_INIT_VIDEO | sdl3.SDL_INIT_EVENTS | sdl3.SDL_INIT_TIMER | sdl3.SDL_INIT_AUDIO)
 
@@ -12,16 +23,23 @@ def main(argv):
     window = sdl3.SDL_CreateWindow("Aermoss".encode(), 1200, 600, sdl3.SDL_WINDOW_RESIZABLE)
 
     renderDrivers = [sdl3.SDL_GetRenderDriver(i).decode() for i in range(sdl3.SDL_GetNumRenderDrivers())]
-    print(f"available render drivers: {", ".join(renderDrivers)}")
+    print(f"available render drivers: {', '.join(renderDrivers)}")
 
-    renderer = sdl3.SDL_CreateRenderer(window, ("vulkan" if "vulkan" in renderDrivers else "software").encode())
+    def tryGetDriver(order, drivers):
+        for i in order:
+            if i in drivers:
+                return i
+
+        return None
+
+    renderer = sdl3.SDL_CreateRenderer(window, tryGetDriver((["vulkan"] if "win32" in sys.platform else []) + ["opengl", "software"], renderDrivers).encode())
 
     if not renderer:
         print(f"failed to create renderer: {sdl3.SDL_GetError().decode().lower()}.")
         return 1
     
     audioDrivers = [sdl3.SDL_GetAudioDriver(i).decode() for i in range(sdl3.SDL_GetNumAudioDrivers())]
-    print(f"available audio drivers: {", ".join(audioDrivers)}")
+    print(f"available audio drivers: {', '.join(audioDrivers)}")
     audioDevices = sdl3.SDL_GetAudioPlaybackDevices(None)
 
     if not audioDevices:
@@ -29,16 +47,33 @@ def main(argv):
         return 1
 
     currentAudioDevice = sdl3.SDL_OpenAudioDevice(audioDevices[0], None)
-    print(f"current audio device: {sdl3.SDL_GetAudioDeviceName(currentAudioDevice).decode().lower()}.")
-
-    audioSpec, audioBuffer, audioSize = sdl3.SDL_AudioSpec(), ctypes.POINTER(ctypes.c_uint8)(), ctypes.c_uint32()
-    sdl3.SDL_LoadWAV("example.wav".encode(), ctypes.byref(audioSpec), ctypes.byref(audioBuffer), ctypes.byref(audioSize))
-    audioStream = sdl3.SDL_CreateAudioStream(ctypes.byref(audioSpec), ctypes.byref(audioSpec))
-    sdl3.SDL_PutAudioStreamData(audioStream, audioBuffer, audioSize.value)
-    sdl3.SDL_BindAudioStream(currentAudioDevice, audioStream)
-    sdl3.SDL_SetAudioStreamFrequencyRatio(audioStream, 1.0)
     
-    running, hue, last = True, 0.0, 0.0
+    if currentAudioDevice:
+        print(f"current audio device: {sdl3.SDL_GetAudioDeviceName(currentAudioDevice).decode().lower()}.")
+
+    sdl3.IMG_Init(sdl3.IMG_INIT_JPG | sdl3.IMG_INIT_PNG)
+    surface = sdl3.IMG_Load("res/example.png".encode())
+    texture = sdl3.SDL_CreateTextureFromSurface(renderer, surface)
+
+    rect = sdl3.SDL_Rect()
+    sdl3.SDL_GetSurfaceClipRect(surface, ctypes.byref(rect))
+
+    frect = sdl3.SDL_FRect()
+    sdl3.SDL_RectToFRect(ctypes.byref(rect), ctypes.byref(frect))
+    running, hue, lastTime, scale = True, 0.0, 0.0, 0.75
+
+    sdl3.TTF_Init()
+    font = sdl3.TTF_OpenFont("res/example.ttf".encode(), 32)
+
+    if currentAudioDevice:
+        sdl3.Mix_Init(sdl3.MIX_INIT_WAVPACK)
+        audioSpec = sdl3.SDL_AudioSpec()
+        sdl3.Mix_OpenAudio(currentAudioDevice, ctypes.byref(audioSpec))
+        chunks = [sdl3.Mix_LoadWAV(f"res/voice/{i}".encode()) for i in os.listdir("res/voice")]
+        currentIndex, channel = 0, 0
+
+    sinceLastFrame, frames, frameCooldown = 0.0, 0.0, 1.0
+    textTexture, textFRect = None, sdl3.SDL_FRect()
 
     while running:
         event = sdl3.SDL_Event()
@@ -52,21 +87,63 @@ def main(argv):
                     if event.key.key == sdl3.SDLK_ESCAPE:
                         running = False
 
-        if not sdl3.SDL_GetAudioStreamAvailable(audioStream):
-            sdl3.SDL_PutAudioStreamData(audioStream, audioBuffer, audioSize.value)
+        width, height = ctypes.c_int(), ctypes.c_int()
+        sdl3.SDL_GetWindowSize(window, width, height)
 
-        last, delta = \
-            time.time(), time.time() - last
+        frect.w, frect.h = frect.w * width.value / frect.w * scale, frect.h * width.value / frect.w * scale
+        frect.x, frect.y = width.value / 2 - frect.w / 2, height.value / 2 - frect.h / 2
 
-        hue += 0.5 * delta
+        if currentAudioDevice and not sdl3.Mix_Playing(channel):
+            channel = sdl3.Mix_PlayChannel(-1, chunks[currentIndex], 1)
+            currentIndex += 1
 
+            if currentIndex >= len(chunks):
+                currentIndex = 0
+
+        lastTime, deltaTime = \
+            time.time(), time.time() - lastTime
+
+        hue, frames = (hue + 0.5 * deltaTime) % 1.0, frames + 1.0
         sdl3.SDL_SetRenderDrawColorFloat(renderer, *colorsys.hsv_to_rgb(hue, 1.0, 0.1), 255.0)
         sdl3.SDL_RenderClear(renderer)
+        sdl3.SDL_RenderTexture(renderer, texture, None, ctypes.byref(frect))
+        sinceLastFrame += deltaTime
+
+        if sinceLastFrame >= frameCooldown:
+            framesPerSecond = int(frames / sinceLastFrame)
+            sinceLastFrame, frames = 0.0, 0.0
+
+            if textTexture:
+                sdl3.SDL_DestroySurface(textSurface)
+                sdl3.SDL_DestroyTexture(textTexture)
+
+            textSurface = sdl3.TTF_RenderText_Solid(font, f"FPS: {framesPerSecond}".encode(), sdl3.SDL_Color(255, 255, 255, 255))
+            textTexture = sdl3.SDL_CreateTextureFromSurface(renderer, textSurface)
+
+            textRect = sdl3.SDL_Rect()
+            sdl3.SDL_GetSurfaceClipRect(textSurface, ctypes.byref(textRect))
+            sdl3.SDL_RectToFRect(ctypes.byref(textRect), ctypes.byref(textFRect))
+
+        sdl3.SDL_RenderTexture(renderer, textTexture, None, ctypes.byref(textFRect))
         sdl3.SDL_RenderPresent(renderer)
 
-    sdl3.SDL_UnbindAudioStream(audioStream)
-    sdl3.SDL_DestroyAudioStream(audioStream)
-    sdl3.SDL_CloseAudioDevice(currentAudioDevice)
+    if textTexture:
+        sdl3.SDL_DestroySurface(textSurface)
+        sdl3.SDL_DestroyTexture(textTexture)
+
+    if currentAudioDevice:
+        for i in chunks:
+            sdl3.Mix_FreeChunk(i)
+
+        sdl3.Mix_CloseAudio()
+        sdl3.Mix_Quit()
+
+    sdl3.TTF_CloseFont(font)
+    sdl3.TTF_Quit()
+
+    sdl3.SDL_DestroySurface(surface)
+    sdl3.SDL_DestroyTexture(texture)
+    sdl3.IMG_Quit()
 
     sdl3.SDL_DestroyRenderer(renderer)
     sdl3.SDL_DestroyWindow(window)
