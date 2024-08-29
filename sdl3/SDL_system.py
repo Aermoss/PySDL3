3a17e5f4f7233c9331f3f4f016fa9c67a1768651
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-08-29 03:05:47 +0300

    Changed platform detection code.

--- sdl3/SDL_system.py
+++ sdl3/SDL_system.py
@@ -5,7 +5,7 @@ from .SDL_video import SDL_DisplayID
 
 SDL_SET_CURRENT_DLL(SDL_DLL)
 
-if "win32" in sys.platform:
+if sys.platform in ["win32"]:
     import ctypes.wintypes as wintypes
 
     MSG = wintypes.tagMSG
@@ -21,7 +21,7 @@ if "win32" in sys.platform:
     SDL_X11EventHook = ctypes.CFUNCTYPE(ctypes.c_bool, ctypes.c_void_p, ctypes.POINTER(XEvent))
     SDL_FUNC("SDL_SetX11EventHook", None, SDL_X11EventHook, ctypes.c_void_p) 
 
-if "linux" in sys.platform:
+if sys.platform in ["linux"]:
     SDL_FUNC("SDL_SetLinuxThreadPriority", ctypes.c_bool, ctypes.c_int64, ctypes.c_int)
     SDL_FUNC("SDL_SetLinuxThreadPriorityAndPolicy", ctypes.c_bool, ctypes.c_int64, ctypes.c_int, ctypes.c_int)
 
