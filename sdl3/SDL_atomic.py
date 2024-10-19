4c24c2903b4f797e67c40ad7be49c6f84b4adcc7
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-10-20 01:21:02 +0300

    Update SDL_atomic.h implementation.

--- sdl3/SDL_atomic.py
+++ sdl3/SDL_atomic.py
@@ -17,20 +17,29 @@ class SDL_AtomicInt(ctypes.Structure):
         ("value", ctypes.c_int)
     ]
 
-SDL_FUNC("SDL_AtomicCompareAndSwap", ctypes.c_bool, ctypes.POINTER(SDL_AtomicInt), ctypes.c_int, ctypes.c_int)
-SDL_FUNC("SDL_AtomicSet", ctypes.c_int, ctypes.POINTER(SDL_AtomicInt), ctypes.c_int)
-SDL_FUNC("SDL_AtomicGet", ctypes.c_int, ctypes.POINTER(SDL_AtomicInt))
-SDL_FUNC("SDL_AtomicAdd", ctypes.c_int, ctypes.POINTER(SDL_AtomicInt), ctypes.c_int)
+SDL_FUNC("SDL_CompareAndSwapAtomicInt", ctypes.c_bool, ctypes.POINTER(SDL_AtomicInt), ctypes.c_int, ctypes.c_int)
+SDL_FUNC("SDL_SetAtomicInt", ctypes.c_int, ctypes.POINTER(SDL_AtomicInt), ctypes.c_int)
+SDL_FUNC("SDL_GetAtomicInt", ctypes.c_int, ctypes.POINTER(SDL_AtomicInt))
+SDL_FUNC("SDL_AddAtomicInt", ctypes.c_int, ctypes.POINTER(SDL_AtomicInt), ctypes.c_int)
 
 class LP_SDL_AtomicInt(ctypes._Pointer):
     ...
 
 def SDL_AtomicIncRef(a: LP_SDL_AtomicInt) -> ctypes.c_int:
-    return SDL_GET_DLL(SDL_DLL).SDL_AtomicAdd(a, 1)
+    return SDL_GET_DLL(SDL_DLL).SDL_AddAtomicInt(a, 1)
 
 def SDL_AtomicDecRef(a: LP_SDL_AtomicInt) -> ctypes.c_int:
-    return SDL_GET_DLL(SDL_DLL).SDL_AtomicAdd(a, -1) == 1
+    return SDL_GET_DLL(SDL_DLL).SDL_AddAtomicInt(a, -1) == 1
 
-SDL_FUNC("SDL_AtomicCompareAndSwapPointer", ctypes.c_bool, ctypes.POINTER(ctypes.c_void_p), ctypes.c_void_p, ctypes.c_void_p)
-SDL_FUNC("SDL_AtomicSetPtr", ctypes.c_void_p, ctypes.POINTER(ctypes.c_void_p), ctypes.c_void_p)
-SDL_FUNC("SDL_AtomicGetPtr", ctypes.c_void_p, ctypes.POINTER(ctypes.c_void_p))
+class SDL_AtomicU32(ctypes.Structure):
+    _fields_ = [
+        ("value", ctypes.c_uint32)
+    ]
+
+SDL_FUNC("SDL_CompareAndSwapAtomicU32", ctypes.c_bool, ctypes.POINTER(SDL_AtomicU32), ctypes.c_uint32, ctypes.c_uint32)
+SDL_FUNC("SDL_SetAtomicU32", ctypes.c_uint32, ctypes.POINTER(SDL_AtomicU32), ctypes.c_uint32)
+SDL_FUNC("SDL_GetAtomicU32", ctypes.c_uint32, ctypes.POINTER(SDL_AtomicU32))
+
+SDL_FUNC("SDL_CompareAndSwapAtomicPointer", ctypes.c_bool, ctypes.POINTER(ctypes.c_void_p), ctypes.c_void_p, ctypes.c_void_p)
+SDL_FUNC("SDL_SetAtomicPointer", ctypes.c_void_p, ctypes.POINTER(ctypes.c_void_p), ctypes.c_void_p)
+SDL_FUNC("SDL_GetAtomicPointer", ctypes.c_void_p, ctypes.POINTER(ctypes.c_void_p))
