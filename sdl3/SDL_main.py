4fa512b385c5af4d05b9a69bc36b174592c86893
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-12-15 19:56:33 +0300

    Fix undefined symbol error on linux.

--- sdl3/SDL_main.py
+++ sdl3/SDL_main.py
@@ -1,4 +1,4 @@
-from .__init__ import ctypes, \
+from .__init__ import sys, ctypes, \
     SDL_FUNC, SDL_SET_CURRENT_DLL, SDL_DLL
 
 from .SDL_init import SDL_AppResult, \
@@ -19,5 +19,6 @@ SDL_FUNC("SDL_SetMainReady", None)
 SDL_FUNC("SDL_RunApp", ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_char_p), SDL_main_func, ctypes.c_void_p)
 SDL_FUNC("SDL_EnterAppMainCallbacks", ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_char_p), SDL_AppInit_func, SDL_AppIterate_func, SDL_AppEvent_func, SDL_AppQuit_func)
 
-SDL_FUNC("SDL_RegisterApp", ctypes.c_bool, ctypes.c_char_p, ctypes.c_uint32, ctypes.c_void_p)
-SDL_FUNC("SDL_UnregisterApp", None)
+if "win32" in sys.platform:
+    SDL_FUNC("SDL_RegisterApp", ctypes.c_bool, ctypes.c_char_p, ctypes.c_uint32, ctypes.c_void_p)
+    SDL_FUNC("SDL_UnregisterApp", None)
