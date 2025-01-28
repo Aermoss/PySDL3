a35619fcb1beaf6eec9e1ba7b5fe84adc5e9781c
Aermoss <86446300+Aermoss@users.noreply.github.com>
2025-01-28 13:10:16 +0300

    Bump version to 'v0.9.3b0'.

--- sdl3/__init__.py
+++ sdl3/__init__.py
@@ -1,6 +1,6 @@
 """A pure Python wrapper for SDL3."""
 
-__version__ = "0.9.2b4"
+__version__ = "0.9.3b0"
 
 import sys, os, requests, ctypes, platform, keyword, inspect, types, re, \
     asyncio, aiohttp, zipfile, typing, array, atexit, packaging.version
