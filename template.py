5fb799bc2764efcfb1f078f4910e50b1a84071bb
Aermoss <86446300+Aermoss@users.noreply.github.com>
2025-01-28 10:07:22 +0300

    Update template.

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os, ctypes, time
 
 os.environ["SDL_MAIN_USE_CALLBACKS"] = "1"
 
-import sdl3
+import sdl3, colorsys
 
 class AppState(ctypes.Structure):
     _fields_ = [
@@ -11,17 +11,16 @@ class AppState(ctypes.Structure):
     ]
 
 def SDL_AppInit(appstate: sdl3.LP_c_void_p, argc: ctypes.c_int, argv: sdl3.LP_c_char_p) -> sdl3.SDL_AppResult:
-    state = AppState()
+    appstate[0] = ctypes.cast(ctypes.pointer(state := AppState()), ctypes.c_void_p)
 
     if not sdl3.SDL_Init(sdl3.SDL_INIT_VIDEO):
-        sdl3.SDL_Log("Couldn't initialize SDL: %s", sdl3.SDL_GetError())
+        sdl3.SDL_Log("Couldn't initialize SDL: ".encode() + sdl3.SDL_GetError())
         return sdl3.SDL_APP_FAILURE
 
     if not sdl3.SDL_CreateWindowAndRenderer("Hello, World!".encode(), 640, 480, 0, ctypes.byref(state.window), ctypes.byref(state.renderer)):
-        sdl3.SDL_Log("Couldn't create window/renderer: %s", sdl3.SDL_GetError())
+        sdl3.SDL_Log("Couldn't create window/renderer: ".encode() + sdl3.SDL_GetError())
         return sdl3.SDL_APP_FAILURE
 
-    appstate[0] = ctypes.cast(ctypes.pointer(state), ctypes.c_void_p)
     return sdl3.SDL_APP_CONTINUE
 
 def SDL_AppEvent(appstate: ctypes.c_void_p, event: sdl3.LP_SDL_Event) -> sdl3.SDL_AppResult:
@@ -32,10 +31,10 @@ def SDL_AppEvent(appstate: ctypes.c_void_p, event: sdl3.LP_SDL_Event) -> sdl3.SD
 
 def SDL_AppIterate(appstate: ctypes.c_void_p) -> sdl3.SDL_AppResult:
     state: AppState = sdl3.SDL_DEREFERENCE(ctypes.cast(appstate, ctypes.POINTER(AppState)))
-    sdl3.SDL_SetRenderDrawColor(state.renderer, int(time.time() * 255 / 2) % 256, 0, 0, sdl3.SDL_ALPHA_OPAQUE)
+    sdl3.SDL_SetRenderDrawColorFloat(state.renderer, *colorsys.hsv_to_rgb(time.time() / 3.0 % 1.0, 1.0, 0.1), sdl3.SDL_ALPHA_OPAQUE_FLOAT)
     sdl3.SDL_RenderClear(state.renderer)
     sdl3.SDL_RenderPresent(state.renderer)
     return sdl3.SDL_APP_CONTINUE
 
 def SDL_AppQuit(appstate: ctypes.c_void_p, result: sdl3.SDL_AppResult) -> None:
-    ... # SDL will clean up the window/renderer for us.
+    ...
