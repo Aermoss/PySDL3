d5c3daa49f2e5e4da41ee28c8ecd64abf188cd44
Aermoss <86446300+Aermoss@users.noreply.github.com>
2025-01-30 19:31:07 +0300

    Disable the doc generator for tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,8 @@ import os, sys, atexit, typing
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+os.environ["SDL_DOC_GENERATOR"] = "0"
+
 import sdl3, ctypes
 
 functions = {}
