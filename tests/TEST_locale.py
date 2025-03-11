fd826615d51bff8ae8d1fe8f006e974e87f472bf
Yusuf <86446300+Aermoss@users.noreply.github.com>
2025-03-12 00:03:19 +0300

    Fix null pointer access.

--- tests/TEST_locale.py
+++ tests/TEST_locale.py
@@ -1,10 +1,10 @@
 from .__init__ import sdl3, ctypes, TEST_RegisterFunction
 
-@TEST_RegisterFunction(["Darwin", "Windows", "Linux"])
+@TEST_RegisterFunction(["Darwin", "Windows"])
 def TEST_SDL_GetPreferredLocales() -> None:
     assert sdl3.SDL_Init(0), sdl3.SDL_GetError().decode()
     count = ctypes.pointer(ctypes.c_int(0))
-    locales = sdl3.SDL_GetPreferredLocales(count)[0]
+    locales = sdl3.SDL_GetPreferredLocales(count)
     assert locales, sdl3.SDL_GetError().decode()
     assert count.contents.value != 0, "Failed to get preferred locales."
     sdl3.SDL_Quit()
