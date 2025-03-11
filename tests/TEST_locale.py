44324bc0157df25b3f134ec758ea34bce66b5c59
Yusuf <86446300+Aermoss@users.noreply.github.com>
2025-03-11 23:48:23 +0300

    Add new tests.

---
+++ tests/TEST_locale.py
@@ -0,0 +1,10 @@
+from .__init__ import sdl3, ctypes, TEST_RegisterFunction
+
+@TEST_RegisterFunction(["Darwin", "Windows", "Linux"])
+def TEST_SDL_GetPreferredLocales() -> None:
+    assert sdl3.SDL_Init(0), sdl3.SDL_GetError().decode()
+    count = ctypes.pointer(ctypes.c_int(0))
+    locales = sdl3.SDL_GetPreferredLocales(count)[0]
+    assert locales, sdl3.SDL_GetError().decode()
+    assert count.contents.value != 0, "Failed to get preferred locales."
+    sdl3.SDL_Quit()
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-import os, sys, typing, collections.abc as abc
+import os, sys, collections.abc as abc
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -6,16 +6,14 @@ os.environ["SDL_DOC_GENERATOR"] = "0"
 
 import sdl3, ctypes, atexit
 
-functions = {}
+functions: dict[abc.Callable[..., None], list[str]] = {}
 
-def TEST_RegisterFunction(systems: list[str]) -> abc.Callable[[abc.Callable[..., typing.Any]], None]:
+def TEST_RegisterFunction(systems: list[str]) -> abc.Callable[[abc.Callable[..., None]], None]:
     return lambda func: functions.update({func: systems})
-
-class TEST_PassFunction(Exception):
-    ...
 
 from tests.TEST_init import *
 from tests.TEST_video import *
+from tests.TEST_locale import *
 
 @atexit.register
 def TEST_RunAllTests() -> None:
@@ -25,14 +23,13 @@ def TEST_RunAllTests() -> None:
 
     for func, systems in functions.items():
         try:
-            sdl3.SDL_ClearError()
-            if sdl3.SDL_SYSTEM in systems: func()
-            else: raise TEST_PassFunction()
-            print("\33[32m", f"Test '{func.__name__}' passed.", "\33[0m", sep = "", flush = True)
-            passed += 1
+            if sdl3.SDL_SYSTEM not in systems:
+                print("\33[33m", f"Test '{func.__name__}' is not supported on {sdl3.SDL_SYSTEM}.", "\33[0m", sep = "", flush = True)
 
-        except TEST_PassFunction as error:
-            print("\33[33m", f"Test '{func.__name__}' is not supported on {sdl3.SDL_SYSTEM}.", "\33[0m", sep = "", flush = True)
+            else:
+                sdl3.SDL_ClearError(); func()
+                print("\33[32m", f"Test '{func.__name__}' passed.", "\33[0m", sep = "", flush = True)
+                passed += 1
 
         except AssertionError as error:
             print("\33[31m", f"Test '{func.__name__}' failed: {error}", "\33[0m", sep = "", flush = True)
