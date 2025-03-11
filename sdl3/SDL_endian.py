3dfd6c546bc6db600b9c5ff5615341c209716633
Yusuf <86446300+Aermoss@users.noreply.github.com>
2025-03-11 23:48:19 +0300

    Update 'SDL_endian.h' implementation.

--- sdl3/SDL_endian.py
+++ sdl3/SDL_endian.py
@@ -4,8 +4,6 @@ SDL_LIL_ENDIAN, SDL_BIG_ENDIAN = 1234, 4321
 SDL_BYTEORDER = SDL_LIL_ENDIAN \
     if sys.byteorder == "little" else SDL_BIG_ENDIAN
 
-sys.byteorder
-
 def SDL_Swap16(x):
     return ((x << 8) & 0xFF00) | ((x >> 8) & 0x00FF)
 
@@ -16,26 +14,13 @@ def SDL_Swap64(x):
     return (SDL_Swap32(x & 0xFFFFFFFF) << 32) | (SDL_Swap32(x >> 32 & 0xFFFFFFFF))
 
 def SDL_SwapFloat(x):
-    ar = array.array("d", (x, ))
-    ar.byteswap()
-    return ar[0]
+    y = array.array("d", (x, ))
+    y.byteswap(); return y[0]
 
 if SDL_BYTEORDER == SDL_LIL_ENDIAN:
-    SDL_SwapLE16 = lambda x: x
-    SDL_SwapLE32 = lambda x: x
-    SDL_SwapLE64 = lambda x: x
-    SDL_SwapFloatLE = lambda x: x
-    SDL_SwapBE16 = SDL_Swap16
-    SDL_SwapBE32 = SDL_Swap32
-    SDL_SwapBE64 = SDL_Swap64
-    SDL_SwapFloatBE = SDL_SwapFloat
+    SDL_SwapBE16, SDL_SwapBE32, SDL_SwapBE64, SDL_SwapFloatBE = SDL_Swap16, SDL_Swap32, SDL_Swap64, SDL_SwapFloat
+    SDL_SwapLE16 = SDL_SwapLE32 = SDL_SwapLE64 = SDL_SwapFloatLE = lambda x: x
 
 else:
-    SDL_SwapLE16 = SDL_Swap16
-    SDL_SwapLE32 = SDL_Swap32
-    SDL_SwapLE64 = SDL_Swap64
-    SDL_SwapFloatLE = SDL_SwapFloat
-    SDL_SwapBE16 = lambda x: x
-    SDL_SwapBE32 = lambda x: x
-    SDL_SwapBE64 = lambda x: x
-    SDL_SwapFloatBE = lambda x: x
+    SDL_SwapLE16, SDL_SwapLE32, SDL_SwapLE64, SDL_SwapFloatLE = SDL_Swap16, SDL_Swap32, SDL_Swap64, SDL_SwapFloat
+    SDL_SwapBE16 = SDL_SwapBE32 = SDL_SwapBE64 = SDL_SwapFloatBE = lambda x: x
